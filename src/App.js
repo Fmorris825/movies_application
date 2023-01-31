07df69b554a1db6799e57fe946e0e74a27703293
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-30 22:05:07 -0600

    Create GetMovieRequest Search Function.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from "./components/MovieList/MovieList";
 import MovieListHeader from "./components/MovieListHeader/MovieListHeader";
 import SearchBox from "./components/SearchBox/SearchBox";
+import { key } from "./api_keys.env";
 
 function App() {
   const [movies, setmovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=${key}`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setmovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
